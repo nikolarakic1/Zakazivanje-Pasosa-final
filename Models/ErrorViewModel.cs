4c5bacbd16237abea047f0996c556ef061cc4cab
Nikola Rakic <=>
2026-06-22 21:14:59 +0200

    Finalna verzija celog projekta

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ZakazivanjeTerminaZaPasose.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
